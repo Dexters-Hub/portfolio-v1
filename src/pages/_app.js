50772a5252866c58f484e0a81dfd533b2040dfc4
Alwin John <alwinjohn231@gmail.com>
2021-07-02 13:16:40 +0530

    changed favicon and updated fonts and titles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import Theme from '../styles/theme';
-
+import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+      <title>Alwin John: Frontend Web Developer and Flutter Developer.</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
