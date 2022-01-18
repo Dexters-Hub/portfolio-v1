d1259ffc77fa25d3312c208f875bd212bd5d54ed
Alwin <alwinjohn231@gmail.com>
2022-01-18 17:40:29 +0530

    updated resume and other projects added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,6 +8,52 @@ export const projects = [
   //   visit: 'https://google.com',
   //   id: 0,
   // },
+
+  {
+    title:'ScholarFox Website',
+    description: "This is a website which provides scholarships, jobs to students in need. The website is built with React, Next.js, Firebase.",
+    image: '/images/1.png',
+    tags: ['React', 'Next.js', 'Firebase'],
+    source: 'https://github.com/Foces-core/job-list',
+    visit: 'https://job-list-acb8e.web.app/',
+    id: 0,
+  },
+  {
+    title:'Shelf Mobile App',
+    description: "An Event/Attendance App for Organizations built using Flutter and Django.",
+    image: '/images/2.png',
+    tags: ['Flutter', 'AWS', 'Django'],
+    source: 'https://github.com/TinkerHub-CEC/Shelf',
+    visit: '#',
+    id: 1,
+  },
+  {
+    title:'Learning Path Website',
+    description: "Learning Path is a virtual library with different paths or roadmaps listed, which a student can follow at any point in their journey of learning tech.",
+    image: '/images/3.png',
+    tags: ['JavaScript', 'Less', 'HTML', 'CSS'],
+    source: 'https://github.com/tinkerhub/TinkerHub-Learning-Paths',
+    visit: 'https://paths.tinkerhub.org/',
+    id: 2,
+  },
+  {
+    title:'IEEE SB CEC Website',
+    description: "IEEE Student Branch College of Engineering Chengannur was formed in mid-1997, with the goal of keeping the students in touch with advancing technology.",
+    image: '/images/4.png',
+    tags: ['React', 'Next.js','Styled Components'],
+    source: 'https://github.com/cecieee/cecieee-frontend',
+    visit: 'https://cecieee-frontend.netlify.app//',
+    id: 3,
+  },
+  {
+    title:'RoboSight Robot',
+    description: "A nursing robot built by IEEE SB CEC SIGHT volunteers funded by IEEE R10 SAC Special Call for Proposals related to Covid - 19 and deployed in Pandalam CSLTC Centre, Kerala, India.",
+    image: '/images/5.png',
+    tags: ['C', 'Arduino'],
+    source: '#',
+    visit: 'https://www.youtube.com/watch?v=jxHJlKDDKxA',
+    id: 4,
+  },
   {
     title: 'V-CEC App',
     description: "This is the official App of CEC to keep students updated with the institution, forums, university notifications, and everything that's happening in every nook and corner of CEC.",
@@ -15,26 +61,8 @@ export const projects = [
     tags: ['Flutter', 'Firebase'],
     source: 'https://github.com/Arjun-Varma2/test',
     visit: 'https://play.google.com/store/apps/details?id=com.cec.proddec',
-    id: 0
+    id: 5,
   },
-  {
-    title: 'Picture-in-Picture',
-    description: "It allows users to watch videos in a floating window so users can keep track of what are they watching when interacting with other sites or applications.",
-    image: '/images/4_1.jpg',
-    tags: ['Html', 'JavaScript', 'CSS'],
-    source: 'https://github.com/Dexters-Hub/picture-in-picture-JavaScript',
-    visit: 'https://dexters-hub.github.io/picture-in-picture-JavaScript/',
-    id: 1
-  },
-  {
-    title: 'Blog-Next.js',
-    description: "Next.js basics by creating a very simple blog app. Code has to be bundled using a bundler like webpack and transformed using a compiler like Babel.",
-    image: '/images/5.png',
-    tags: ['React', 'Next.js'],
-    source: 'https://github.com/Dexters-Hub/nextjs-blog',
-    visit: 'https://nextjs-blog-nine-plum.vercel.app/',
-    id: 2
-  }
   
   
 ];
